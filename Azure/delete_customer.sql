a7f306d0eea0355092619115974795ca65571f45
Patrick Degner <108484798+PatrickDegner@users.noreply.github.com>
2023-01-12 20:59:01 +0100

    Create delete_customer.sql

---
+++ Azure/delete_customer.sql
@@ -0,0 +1,6 @@
+-- delete bob johnson
+DELETE FROM banking_details
+WHERE customer_id = (SELECT id FROM customer WHERE email = 'bobjohnson@example.com');
+
+DELETE FROM customer
+WHERE email = 'bobjohnson@example.com';
